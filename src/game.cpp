1af397e086ced6b1b9d2575b520e5e5e00c8ae34
lcscarlet <seaoflcl@BOTENANNA>
2026-03-23 05:17:33 -0400

    Add project files.

---
+++ src/game.cpp
@@ -0,0 +1,38 @@
+#include "game.h"
+
+Game::Game() {
+	// TODO! 
+}
+
+Game::~Game() {
+	// TODO! 
+}
+
+void Game::Initialize() {
+	// TODO! 
+}
+
+void Game::Run() {
+		// TODO!
+	while (true) {
+		ProcessInput();
+		Update();
+		Render();
+	}
+}
+
+void Game::ProcessInput() {
+	// TODO! 
+}	
+
+void Game::Update() {
+	// TODO! 
+}
+
+void Game::Render() {
+	// TODO! 
+}
+
+void Game::Destroy() {
+	// TODO! 
+}
