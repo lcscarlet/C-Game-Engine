51f1f279c0e93981cb97201c1f0dd721fe92bf3f
Lee <13585828+lcscarlet@users.noreply.github.com>
2026-03-24 09:46:33 +0000

    Add files via upload
    More work on rendering loop, and removed primitive drawings

--- src/game.cpp
+++ src/game.cpp
@@ -1,7 +1,11 @@
 #include "game.h"
+#include <SDL.h>
+#include <iostream>
+#include <SDL_image.h>
+#include <glm/glm.hpp>
 
 Game::Game() {
-	// TODO! 
+	isRunning = false;
 }
 
 Game::~Game() {
@@ -9,12 +13,44 @@ Game::~Game() {
 }
 
 void Game::Initialize() {
-	// TODO! 
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+	std::cerr << "Error initalizing SDL." << std::endl;
+		return;
+	}
+	SDL_DisplayMode displayMode;
+	SDL_GetCurrentDisplayMode(0, &displayMode);
+	windowWidth = 800; //displayMode.w;
+	windowHeight = 600; //displayMode.h;
+
+	window = SDL_CreateWindow(
+		NULL, 
+		SDL_WINDOWPOS_CENTERED, 
+		SDL_WINDOWPOS_CENTERED,
+		windowWidth,
+		windowHeight,
+		SDL_WINDOW_BORDERLESS);
+	if (!window) {
+		std::cerr << "Error creating SDL window." << std::endl;
+		return;
+	}
+	
+	// SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+	// might want to change this later to use vsync and hardware acceleration
+	 renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		std::cerr << "Error creating SDL renderer." << std::endl;
+		return;
+	}
+
+	//change this later to 80% of current display mode resolution
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+	
+	isRunning = true;
 }
 
 void Game::Run() {
-		// TODO!
-	while (true) {
+	Setup();
+	while (isRunning) {
 		ProcessInput();
 		Update();
 		Render();
@@ -22,17 +58,57 @@ void Game::Run() {
 }
 
 void Game::ProcessInput() {
-	// TODO! 
+	SDL_Event sdlEvent;
+	while (SDL_PollEvent(&sdlEvent))
+		switch (sdlEvent.type) {
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		case SDL_KEYDOWN:
+			if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
+				isRunning = false;
+			break;
+
+		};
 }	
 
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+void Game::Setup(){
+	playerPosition = glm::vec2(10.0, 20.0);
+	playerVelocity = glm::vec2(0.5, 0.0);
+}
+
 void Game::Update() {
-	// TODO! 
+	playerPosition.x += playerVelocity.x;
+	playerPosition.y += playerVelocity.y;
 }
 
 void Game::Render() {
-	// TODO! 
+	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+	SDL_RenderClear(renderer);
+
+	//PNG texture loading and rendering will go here
+	SDL_Surface* surface = IMG_Load(".assets/images/tank-tiger-right.png");
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+
+	//Destination rectangle to place a texture
+	SDL_Rect dstRect = {static_cast<int>(playerPosition.x), 
+						static_cast<int>(playerPosition.y), 
+						32, 
+						32 };
+
+	SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+
+	SDL_DestroyTexture(texture);
+
+	SDL_RenderPresent(renderer);
 }
 
 void Game::Destroy() {
-	// TODO! 
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
--- src/game.h
+++ src/game.h
@@ -1,19 +1,30 @@
 #pragma once
 #ifndef GAME_H
 #define GAME_H
+#include <SDL.h>
+
 
 class Game {
 private:
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 
 public:
 	Game();
 	~Game();
 	void Initialize();
 	void Run();
+	void Setup();
 	void ProcessInput();
 	void Update();
 	void Render();
 	void Destroy();
+
+	//might want to change this to uint or size_t later on
+	int windowWidth;
+	int windowHeight;
+
 };
 
 #endif
